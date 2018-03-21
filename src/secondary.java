c4f3ac6a5700dbb1153ae275b1717dee90929389
Luke Charles <lukecharles222@gmail.com>
2018-03-21 21:50:25 +0000

    transfer of variables

--- src/secondary.java
+++ src/secondary.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class secondary{
     //secondary class with secondary module which numbers are passed into by jump object
     public void secondary(int w, int x, int y){
@@ -8,12 +9,12 @@ public class secondary{
            System.out.println(total * add);
         }
     }
-
+    //incrememnt function which will work out what number the user has put into the program
     public void increment(int w, int x, int y) {
         Double component = 1.9;
         Double component1 = 2.5;
         int less = 0;int more = 0;int exactly = 0;
-        
+        secondary jump2 = new secondary();
         for(int i = 0 ; i < w ; i++){
             if(w<=0){
                 less--;
@@ -22,7 +23,13 @@ public class secondary{
             }else{
                 exactly++;
             }
-            jump.resolution();
+            jump2.resolution(less, exactly, more);
+        }
+    }
+
+    private void resolution(int l, int e, int m) {
+        if(){
+
         }
     }
 }
